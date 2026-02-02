99386e01ed25f04ac7d5a832d2c7de2bfef9a89c
Jeremiah Folorunso <jfolorunso@softalliance.co.ng>
2026-02-02 11:56:33 +0100

    new article desc1

---
+++ src/constant/single-blog/blog12.ts
@@ -0,0 +1,179 @@
+
+export const blog12 = {
+    id: "9871ae2b-64b2-4492-8a87-efb96ccc1012",
+    slug: "whole-creative-1",
+    author: "Jeremiah Folorunso",
+    authorRole: "Product Designer",
+    authorImage: "/images/author-image.jpg",
+    title: "A Balanced and Whole Creative (The Meaning - Part 1)",
+    body: `
+    
+    <div style="max-width: 700px; margin: 3rem auto; text-align: center;">
+    <p style="font-style: italic; color: #4a4a4a; ">
+        On Becoming a Whole and Sound Creative.
+    </p>
+    </div>
+    
+
+    <div style="max-width: 700px; margin: 3rem auto; text-align: center;">
+    <p style="font-style: italic; color: #4a4a4a; ">
+        ----------
+    </p>
+    </div>
+
+    <p>
+    In our world today, you would agree with me that … <strong> Success is often measured by output, visibility, and momentum. </strong>
+    </p>
+
+
+    <p>
+    While this approach can yield results, it often leaves little room to ask deeper 
+    and <em> maybe more profound </em> questions about sustainability, identity, and maybe our internal health. 
+    </p>
+
+    <p>
+    I have had this series in mind for a while, and the goal is to use it to try and shift something in us. 
+    With it, we’ll explore what it means to be a wholesome creative, that is, 
+    one whose growth is not only visible on the outside but stable on the inside.
+    </p>
+    
+    <p><em>Sounds serious shey, i know.</em> </p>
+
+    <p>One thing to keep in mind is that, long-term career and self impact
+    is not solely based on productivity; it also requires wholeness.</p>
+
+    <p>
+    Sooo, let’s start 😎
+    </p>
+
+    <div style="max-width: 700px; margin: 3rem auto; text-align: center;">
+    <p style="font-style: italic; color: #4a4a4a; ">
+        ----------
+    </p>
+    </div>
+
+
+    <h2>
+    What Does a Balanced and Whole Creative Look Like?
+    </h2>
+
+    <p>
+    To make this practical, we need a yardstick. 
+    Something we can actually reflect on and measure our wholeness with. 
+    Here are a few characteristics that are commonly found in a balanced and whole creative individual:
+    </p>
+
+    <ul>
+        <li> <strong>The person is at Peace while being productive -</strong> The ability to rest without guilt or anxiety.</li>
+        <li> <strong>The person’s Consistency has a measured level of intensity -</strong> A pace that can be sustained beyond short bursts of effort.</li>
+        <li> <strong>There is strong Alignment between values and output -</strong> Conviction is seen in their work/deliverables.</li>
+        <li> <strong>Their Identity is beyond only results.</strong> </li>
+        <li> <strong>They posses good Emotional and mental stability.</strong> </li>
+    </ul>
+
+    <p>
+    These indicators/characteristics may not appear on dashboards, but they determine longevity. 
+    In short, they are almost not measured with numbers only through deep reflections.
+    <p>
+
+    <div style="max-width: 700px; margin: 3rem auto; text-align: center;">
+    <p style="font-style: italic; color: #4a4a4a; ">
+        ----------
+    </p>
+    </div>
+    
+    <h2>
+    It’s Important to note that wholeness isn’t the absence of struggle
+    </h2>
+
+    <p>
+    Oh yesss, wholeness does not mean a life free from pressure, 
+    doubt, or fatigue. Struggle is an inevitable part of growth and creative work.
+    </p>
+
+    <p>
+    What distinguishes a balanced/whole creative from another <em<(for fear of using the word “broken”)</em> 
+    is not the absence of difficulty, but the capacity to carry it without internal collapse. 
+    Wholeness allows tension without disintegration.
+    </p>
+
+    <p>
+    A <strong> balanced/whole creative </strong> can encounter pressure without losing their stability. 
+    Not the one that gives up on work or productive things by tuesday.
+    </p>
+
+    <p>
+    <em> #comic relief below 😆 </em>
+    </p>
+
+    <div>
+        <img 
+            src="/images/Img_12.webp" 
+            alt="Blog Image"
+            style="
+            display: block;
+            width: 75vw;
+            max-width: 100%;
+            object-fit: cover;
+            margin: 0 auto;
+            border-radius: 6px;
+            "
+        >
+    </div>
+
+    <div style="max-width: 700px; margin: 0rem auto; text-align: center;">
+    <p style="font-style: italic; color: #ffffff; ">
+        ----------
+    </p>
+    </div>
+  
+    <p>
+    This first part of this series is about definition, not resolution
+    </p>
+
+    <p>
+    In the next part, we will shift focus toward identity and ask a necessary question: 
+    <strong> Using deep reflection, how close am i to being a balanced/whole creative? </strong>
+    </p>
+    
+    <p>
+    With you,
+    </p>
+    <p style="font-style: bold;">
+    Jeremiah
+    </p>
+
+    <div style="max-width: 700px; margin: 3rem auto; text-align: center;">
+    <p style="font-style: italic; color: #4a4a4a; ">
+        ----------
+    </p>
+    </div>
+
+     <p>
+    P.S: If this resonated with you, share it with a creative friend who might be bumping up against their own ceiling.
+    </p>
+    
+    <p> Also, let's stay connected on: <a href="https://www.instagram.com/hellocre8tives">Instagram</a> || 
+    <a href="https://www.twitter.com/hellocre8tives">X(formerly Twitter)</a> </p>
+
+    <div>
+        <img 
+            src="/images/Img_12a.webp" 
+            alt="Blog Image"
+            style="
+            display: block;
+            width: 75vw;
+            max-width: 100%;
+            object-fit: cover;
+            margin: 0 auto;
+            border-radius: 6px;
+            "
+        >
+    </div>
+
+    `,
+    date: "2 February, 2026",
+    readingTime: "3 minutes read",
+    summary: "An Wholesome Creative",
+    blogBrief: "Exporting Your Creative Talent Beyond Borders.",
+}
